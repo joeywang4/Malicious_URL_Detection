c9e279b020c72b4460ba83c7e07e942784bdd471
WangYuNeng <33870148+WangYuNeng@users.noreply.github.com>
2018-06-10 22:53:55 +0800

    Add files via upload

--- dbd.py
+++ dbd.py
@@ -2,6 +2,69 @@ from pyjsparser import PyJsParser
 import requests
 from bs4 import BeautifulSoup as bs
 
+subFuncDict = {}
+FuncCallTimes = {}
+#funcDeclare：現在在那些FunctionDeclartion下
+funcDeclare = []
+
+def addFuncCallTimes(funcname):
+    if funcname not in FuncCallTimes:
+        FuncCallTimes[funcname] = 1
+    else:
+        FuncCallTimes[funcname] += 1
+    if funcname in subFuncDict:
+        for subfunc in subFuncDict[funcname]:
+            if subfunc not in FuncCallTimes:
+                FuncCallTimes[subfunc] = subFuncDict[funcname][subfunc]
+            else:
+                FuncCallTimes[subfunc] += subFuncDict[funcname][subfunc]
+    #把自己和subfunc都加+=1
+
+def addsubFuncToFunc(subfuncname):
+    for func in funcDeclare:
+        if func not in subFuncDict:
+            subFuncDict[func] = {}
+            subFuncDict[func][subfuncname] = 1
+        else:
+            if subfuncname not in subFuncDict[func]:
+                subFuncDict[func][subfuncname] = 1
+            else:
+                subFuncDict[func][subfuncname] += 1
+
+
+def read_dic(dic):
+    if not isinstance(dic, dict):
+        return
+    if dic['type'] == 'CallExpression':
+        funcname = ""
+        tmp = dic['callee']
+        while tmp['type'] != 'Identifier':
+            funcname = tmp['property']['name'] + '.' + funcname
+            tmp = tmp['object']
+        funcname = tmp['name'] + '.' + funcname
+        addFuncCallTimes(funcname[:-1])
+        addsubFuncToFunc(funcname[:-1])
+        return
+    elif dic['type'] == 'FunctionDeclaration':
+        funcDeclare.append(dic['id']['name'])
+    for key, val in dic.items():
+        if isinstance(val, dict):
+            read_dic(val)
+        elif isinstance(val, list):
+            read_list(val)
+    return
+
+def read_list(li):
+    if not isinstance(li, list):
+        return
+    for item in li:
+        if isinstance(item, dict):
+            read_dic(item)
+        elif isinstance(item, list):
+            read_list(item)
+    return
+
+
 class js_detect:
     url = ""
     debug = True
@@ -31,6 +94,8 @@ class js_detect:
 
         p = PyJsParser()
         a = p.parse(tot_script)
-        for l in a['body']:
-            print(l)
-            print("")
+        read_dic(a)
+        print(subFuncDict)
+        print()
+        print()
+        print(FuncCallTimes)
--- main.py
+++ main.py
@@ -2,22 +2,58 @@ from phishing import phish_detect
 from dbd import js_detect
 from pyjsparser.parser import PyJsParser
 
-'''
+
 js_detect("https://ceiba.ntu.edu.tw/index.php", True)
+
+
 '''
+
+def addFuncCallTimes(funcname):
+    if funcname not in FuncCallTimes:
+        FuncCallTimes[funcname] = 1
+    else:
+        FuncCallTimes[funcname] += 1
+    if funcname in subFuncDict:
+        for subfunc in subFuncDict[funcname]:
+            if subfunc not in FuncCallTimes:
+                FuncCallTimes[subfunc] = subFuncDict[funcname][subfunc]
+            else:
+                FuncCallTimes[subfunc] += subFuncDict[funcname][subfunc]
+    #把自己和subfunc都加+=1
+
+def addsubFuncToFunc(subfuncname):
+    for func in funcDeclare:
+        if func not in subFuncDict:
+            subFuncDict[func] = {}
+            subFuncDict[func][subfuncname] = 1
+        else:
+            if subfuncname not in subFuncDict[func]:
+                subFuncDict[func][subfuncname] = 1
+            else:
+                subFuncDict[func][subfuncname] += 1
+
+
 def read_dic(dic):
     if not isinstance(dic, dict):
         return
     if dic['type'] == 'CallExpression':
-        func = ""
+        funcname = ""
         tmp = dic['callee']
-        while tmp['type'] is "MemberExpression":
-            func += "1"
+        while tmp['type'] != 'Identifier':
+            funcname = tmp['property']['name'] + '.' + funcname
+            tmp = tmp['object']
+        funcname = tmp['name'] + '.' + funcname
+        addFuncCallTimes(funcname[:-1])
+        addsubFuncToFunc(funcname[:-1])
+        return
+    elif dic['type'] == 'FunctionDeclaration':
+        funcDeclare.append(dic['id']['name'])
     for key, val in dic.items():
         if isinstance(val, dict):
             read_dic(val)
         elif isinstance(val, list):
             read_list(val)
+    return
 
 def read_list(li):
     if not isinstance(li, list):
@@ -27,10 +63,15 @@ def read_list(li):
             read_dic(item)
         elif isinstance(item, list):
             read_list(item)
+    return
+
 
 p = PyJsParser()
 with open('test.js', 'r') as f:
     tot_script = f.read()
     a = p.parse(tot_script)
     print(a)
-    #read_dic(a)
+    read_dic(a)
+    print(subFuncDict)
+    print(FuncCallTimes)
+'''
