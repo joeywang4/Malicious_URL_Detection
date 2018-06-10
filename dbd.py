84a973192c208dfb1730df85b2d7e188c58f81cb
joeywang4 <wangjoey4@gmail.com>
2018-06-10 20:38:12 +0800

    😇

--- dbd.py
+++ dbd.py
@@ -2,26 +2,35 @@ from pyjsparser import PyJsParser
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://ceiba.ntu.edu.tw/index.php"
-debug = True
+class js_detect:
+    url = ""
+    debug = True
+    call_count = list()
+    parsed = ""
 
-if debug:
-    print("Requesting Site...")
-    try:
-        r = requests.get(url, allow_redirects=True)
-        for i in r.history:
-            print("Reditecting from:",i.url, "...")
-    except:
-        print("This site seems to be offline...")
-        exit()
+    def stat(self):
+        call_count = []
+        return
 
-soup = bs(r.text, 'html.parser')
-tot_script = ""
-for script in soup.find_all('script'):
-    tot_script += script.get_text()
+    def __init__(self, url, debug):
+        self.url = url
+        if debug:
+            print("Requesting Site...")
+        try:
+            r = requests.get(url, allow_redirects=True)
+            for i in r.history:
+                print("Reditecting from:",i.url, "...")
+        except:
+            print("This site seems to be offline...")
+            return
 
-p = PyJsParser()
-a = p.parse(tot_script)
-for l in a['body']:
-    print(l)
-    print("")
+        soup = bs(r.text, 'html.parser')
+        tot_script = ""
+        for script in soup.find_all('script'):
+            tot_script += script.get_text()
+
+        p = PyJsParser()
+        a = p.parse(tot_script)
+        for l in a['body']:
+            print(l)
+            print("")
--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from phishing import phish_detect
+from dbd import js_detect
+from pyjsparser.parser import PyJsParser
 
+'''
+js_detect("https://ceiba.ntu.edu.tw/index.php", True)
+'''
+def read_dic(dic):
+    if not isinstance(dic, dict):
+        return
+    if dic['type'] == 'CallExpression':
+        func = ""
+        tmp = dic['callee']
+        while tmp['type'] is "MemberExpression":
+            func += "1"
+    for key, val in dic.items():
+        if isinstance(val, dict):
+            read_dic(val)
+        elif isinstance(val, list):
+            read_list(val)
 
-with open("site_list", 'r') as sites:
-    for url in sites:
-        if url[-1:] == '\n':
-            url = url[:-1]
-        phish_detect(url, True)
+def read_list(li):
+    if not isinstance(li, list):
+        return
+    for item in li:
+        if isinstance(item, dict):
+            read_dic(item)
+        elif isinstance(item, list):
+            read_list(item)
+
+p = PyJsParser()
+with open('test.js', 'r') as f:
+    tot_script = f.read()
+    a = p.parse(tot_script)
+    print(a)
+    #read_dic(a)
