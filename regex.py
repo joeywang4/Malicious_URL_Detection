96aa1d28f8a088cbe587a9171646fff59d24fbf6
joeywang4 <wangjoey4@gmail.com>
2018-06-14 14:26:02 +0800

    add regex matching

---
+++ regex.py
@@ -0,0 +1,22 @@
+import re
+
+class regex:
+    pattern_tuple = list()
+
+    def __init__(self):
+        with open("regex.txt", 'r') as f:
+            for line in f:
+                s = line.split(maxsplit=1)
+                if len(s) == 2 and s[0][0] != '#':
+                    if s[1][-1:] == '\n':
+                        s[1] = s[1][:-1]
+                    try:
+                        self.pattern_tuple.append( (s[0], re.compile(s[1])) )
+                    except:
+                        print("Error parsing regex file of line: {}".format(line))
+
+    def match(self, text):
+        for pat in self.pattern_tuple:
+            if pat[1].match(text) is not None:
+                return pat[0]
+        return None
